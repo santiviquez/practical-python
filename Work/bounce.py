7a0fa83e3dee99ddaff86264cfcfd673fbd6da70
Santiago Víquez <santi.viquez@gmail.com>
2025-01-25 22:16:28 -0600

    excercises 1.2

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100
+bounce_number = 1
+
+while bounce_number <= 10:
+    height = height * 3/5
+    # print(bounce_number, height)
+    print(bounce_number, round(height, 4))
+    bounce_number = bounce_number + 1
